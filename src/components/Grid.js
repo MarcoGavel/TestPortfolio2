858596a20f6dec6a8621ff059a7d4353731c846c
Gavel <156299795+MarcoGavel@users.noreply.github.com>
2024-06-05 00:11:08 +0200

    Update Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -24,7 +24,7 @@ const Grid = () => {
               <h1> Here to Learn</h1>
                 <h2>My journey started at the beginning of 2024</h2>
                 <button class="button" onclick="window.location.reload()">Don't like this image?</button> 
-                <h2>My first certificate [31 May '24]: <a href="https://www.udemy.com/certificate/UC-fa4f4d28-4658-4ad4-8dfb-85d9253ed0eb/">Certificate Link</a></h2>
+                <p>My first certificate --31 May '24--:</p><a href="https://www.udemy.com/certificate/UC-fa4f4d28-4658-4ad4-8dfb-85d9253ed0eb/" className="button mb-2 p-2 target="_blank" rel="noopener noreferrer">Certificate Link</a>
             </div>
           </div>
         </div>
