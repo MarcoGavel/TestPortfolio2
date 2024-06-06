df6e781ad6280a0919cf0fa07ebb8015da392d8a
Gavel <156299795+MarcoGavel@users.noreply.github.com>
2024-06-06 12:04:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import './styles/style.css';
+import './src/styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
